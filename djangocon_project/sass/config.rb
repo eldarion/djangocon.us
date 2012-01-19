115a580e59a6f84ae7d303a4a9014ac83824dc83
Yulka Plekhanova <yulka@insatsu.us>
2012-01-20 00:10:07 +0100

    edited config

--- djangocon_project/sass/config.rb
+++ djangocon_project/sass/config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+
 # Set this to the root of your project when deployed:
 http_path = "../"
 css_dir = "../static/css"
@@ -6,8 +7,6 @@ sass_dir = "src"
 images_dir = "../static/img"
 javascripts_dir = "../static/js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -24,4 +23,3 @@ line_comments = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
